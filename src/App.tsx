473009b0f396added2a810202be419e8b6dc34b5
Kardelen Demir <krdlndmr.1245@gmail.com>
2026-03-25 17:06:17 +0300

    init: create vite project base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: [Student Name]</p>
+      <p>Ogrenci No: [Student Number]</p>
+    </div>
+  )
+}
+
+export default App
